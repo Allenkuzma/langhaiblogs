8035a223ba79eeeb421a281a446c5fb09bc3371b
langhai <648798558@qq.com>
2022-11-19 16:04:22 +0800

    feat：浪海博客项目构建初始化

---
+++ src/main/java/cc/langhai/BlogsApplication.java
@@ -0,0 +1,17 @@
+package cc.langhai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 博客程序启动器
+ *
+ * @author langhai
+ * @date 2022-11-19 16:01
+ */
+@SpringBootApplication
+public class BlogsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsApplication.class, args);
+    }
+}
